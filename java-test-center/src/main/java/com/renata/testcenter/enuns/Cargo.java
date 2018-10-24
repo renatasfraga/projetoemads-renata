e81ebb1c094b98145b369631c836674b9b25326c
Renata dos Santos Fraga <renatasfraga@gmail.com>
2018-10-23 23:27:04 -0300

    Inclusao de arquivo Apresentacao do seminario de andamento. Apresentacao do dia 24/out

--- java-test-center/src/main/java/com/renata/testcenter/enuns/Cargo.java
+++ java-test-center/src/main/java/com/renata/testcenter/enuns/Cargo.java
@@ -2,32 +2,14 @@ package com.renata.testcenter.enuns;
 
 public enum Cargo {
 
-	TEST("TEST","Testador"),
-	ANLTEST("ANLTEST","Analista de Testes"),
-	ANLQUAL("ANLQUAL","Analista de Qualidade"),
-	QA("QA","Quality Assurence"),
-	DEV("DEV","Desenvolvedor"),
-	PO("PO","Product Owner"),
-	ANLSIS("ANLSIS","Analista de Sistemas"),
-	ANLNEG("ANLNEG","Analista de Negócios"),
-	GP("GP","Gerente de Projetos"),
-	LID("LID","Líder");
-	
-	private String codigo;
-	private String descricao;
-	
-	private Cargo(String codigo, String descricao) {
-		this.codigo = codigo;
-		this.descricao = descricao;
-	}
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-	
-	
+	TEST,
+	ANLTEST,
+	ANLQUAL,
+	QA,
+	DEV,
+	PO,
+	ANLSIS,
+	ANLNEG,
+	GP,
+	LID;
 }
--- java-test-center/src/main/java/com/renata/testcenter/enuns/Parecer.java
+++ java-test-center/src/main/java/com/renata/testcenter/enuns/Parecer.java
@@ -1,24 +1,7 @@
 package com.renata.testcenter.enuns;
 
 public enum Parecer {
-	I("I","Insatisfeito"),
-	P("P","Parcialmente Satisfeito"),
-	S("S","Satisfeito");
-	
-	private String codigo;
-	private String descricao;
-	
-	private Parecer(String codigo, String descricao) {
-		this.codigo = codigo;
-		this.descricao = descricao;
-	}
-
-	public String getCodigo() {
-		return codigo;
-	}
-
-	public String getDescricao() {
-		return descricao;
-	}
-	
+	I,
+	P,
+	S;
 }
